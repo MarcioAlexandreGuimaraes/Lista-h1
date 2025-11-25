e5a586dd5a9dd6c13618e508edaad3e00b9aad45
MarcioAlexandreGuimaraes <marcio.guimaraes2615@gmail.com>
2025-11-24 21:43:55 -0300

    Create Matricula class for student enrollment

---
+++ Matrícula.cs
@@ -0,0 +1,7 @@
+public class Matricula
+{
+    public int IdAluno { get; set; }
+    public int IdCurso { get; set; }
+    public DateTime DataMatricula { get; set; }
+    public bool Ativa { get; set; }
+}
