9f562bbe52d5c376c9abdfc0c8411bfe8f061ec4
MarcioAlexandreGuimaraes <marcio.guimaraes2615@gmail.com>
2025-11-24 21:43:20 -0300

    Create Curso.cs

---
+++ Curso.cs
@@ -0,0 +1,7 @@
+public class Curso
+{
+    public int IdCurso { get; set; }
+    public string Nome { get; set; }
+    public string NomeCoordenador { get; set; }
+    public bool Ativo { get; set; }
+}
